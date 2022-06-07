506f54f548c3d8d1aa2011c130973b816664d4e4
Elvis Silva <elviscarlossouza@gmail.com>
2022-06-07 09:36:46 -0300

    update skills component and test

--- src/__tests__/Skills.test.tsx
+++ src/__tests__/Skills.test.tsx
@@ -6,3 +6,21 @@ test('show skill component in document', () => {
   let skill = screen.getByTestId(/skill/i)
   expect(skill).toBeInTheDocument()
 })
+
+test('show container in document', () => {
+  render(<Skills />)
+  let container = screen.getByTestId(/container/i)
+  expect(container).toBeInTheDocument()
+})
+
+test('show title in document', () => {
+  render(<Skills />)
+  let title = screen.getByTestId(/title/i)
+  expect(title).toBeInTheDocument()
+})
+
+test('show text in document', () => {
+  render(<Skills />)
+  let text = screen.getByTestId(/text/i)
+  expect(text).toBeInTheDocument()
+})
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -4,13 +4,13 @@ import { SkillsBody, SkillsContainer, Text } from "./style"
 const Skills = () => {
   return (
     <SkillsBody data-testid='skills'>
-      <SkillsContainer>
-        <StyledHeading>Skills</StyledHeading>
-        <Text>
+      <SkillsContainer data-testid='container'>
+        <StyledHeading data-testid='title'>Skills</StyledHeading>
+        <Text data-testid='text'>
           Sou programador há 5 anos e sei programar em Java, Typescript, Javascript, HTML5, CSS3, ReactJS, controle de versão com Git e Github, gerenciamento de Banco de Dados com MySQL. 
           <br />
           <br />
-          Possuo conhecimento em Estruturas de Dados, Algoritmos e Princípios OOP. Também posso configurar e gerenciar um sistema operacional Windows ou Linux. Tenho mais de 10 anos de experiência com Linux nas mais diversas versões.
+          Possuo conhecimento em Estruturas de Dados, Algoritmos e Princípios POO. Também posso configurar e gerenciar um sistema operacional Windows ou Linux. Tenho mais de 10 anos de experiência com Linux nas mais diversas versões.
           <br />
           <br />
           Também fiz e certifiquei vários bootcamps na plataforma da Digital Innovation One Inc. que podem ser verificados no meu&nbsp;<a color="#65d865" href='https://www.linkedin.com/in/elvis-silva-dev'>Linkedin</a>
