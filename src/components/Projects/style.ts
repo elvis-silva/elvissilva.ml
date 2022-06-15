20cef2750b3f77e7de85ef72746c55507e8fbc50
Elvis Silva <elviscarlossouza@gmail.com>
2022-06-15 12:19:07 -0300

    Update favicon

--- src/components/Projects/style.ts
+++ src/components/Projects/style.ts
@@ -7,7 +7,6 @@ export const ProjectsBody = styled.div`
 
   @media screen and (max-width: 768px) {
     flex-direction: column;
-    align-items: center;
     margin-top: -4em;
   }
 `
@@ -21,4 +20,92 @@ export const VideoHolder = styled.div`
     opacity: 30%;
   }
 
+`
+
+export const Video = styled.video`
+
+  width: 35rem;
+  height: 20rem;
+  min-width: 100%;
+  min-height: 100%;
+  border-radius: 5px;
+  object-fit: fill;
+
+`
+
+export const DescriptionSide = styled.div<{ inverted: boolean }>`
+
+  display: flex;
+  flex-direction: column;
+  align-items: 'flex-end';
+  justify-content: center;
+  width: 25rem;
+  height: 20rem;
+  z-index: 20;
+
+  ${({ inverted }) => inverted === false && `
+
+    align-items: 'flex-start';
+  
+  `}
+
+  @media screen and (max-width: 768px) {
+    position: absolute;
+    align-items: flex-start;
+  }
+
+`
+
+export const ProjectName = styled.h1<{ inverted: boolean }>`
+
+  padding-bottom: 1em;
+  padding-right: 0.5em;
+
+  ${({ inverted }) => inverted === false && `
+  
+    padding-left: 0.5em;
+    padding-right: 0;
+  
+  `}
+
+  @media screen and (max-width: 768px) {
+    padding-bottom: 1em;
+    padding-left: 0.5em;
+  }
+
+`
+
+export const Description = styled.p`
+
+  padding: 1.5em;
+  width: 26rem;
+  border-radius: 5px;
+  font-size: 0.9rem;
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+    padding-left: 1em;
+  }
+
+`
+
+export const TechStack = styled.div`
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding-top: 1em;
+
+`
+
+export const TechStackItem = styled.div`
+
+  padding: 0.5em 0 0.5em 0.5em;
+  font-size: 0.8em;
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+    padding-left: 1.2em;
+    font-weight: bold;
+  }
 `
